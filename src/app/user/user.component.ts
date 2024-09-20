b8e1d0812824239720e6b754c711c2648a69ae0c
VinayPanchal1974 <vinaypanchal115@gmail.com>
2024-09-20 14:23:07 +0530

    added input as Signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, signal, computed} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users'
+import { Component, computed, Input, input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -10,14 +9,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
-userImage = computed(()=> '/assets/users/' + this.selectedUser().avatar)
-  // get userImage(){
-  //   return '/assets/users/' + this.selectedUser.avatar
-  // }
-
-  onSelectUser = ()=>{
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex]); 
+  @Input({required:true}) name!: string;
+  @Input({required:true}) avatar!: string;
+  get userImage() {
+    return '/assets/users/' + this.avatar
   }
+
+  // // Signal Way
+  // name = input.required<string>();
+  // avatar = input<string>("default value");
+  // userImage = computed(() => {
+  //   return '/assets/users/' + this.avatar()
+  // })
+
+  onSelectUser = () => { }
 }
