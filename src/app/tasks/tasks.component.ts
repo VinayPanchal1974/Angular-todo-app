a655afc3474b4ab3e0e9a6a122488823937d1247
VinayPanchal1974 <vinaypanchal115@gmail.com>
2024-10-03 12:19:32 +0530

    finished the first angular app

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component'
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,39 +14,20 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   // @Input() name: string | undefined;      // the sign '|' creates a union
+  isAddingTask = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
+  constructor(private tasksService: TasksService) { }
+
 
   get selectedUserTask() {
-    return this.tasks.filter(task => task.userId === this.userId)
+    return this.tasksService.getUserTasks(this.userId)
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(e => e.id !== id)
+ 
+  onStartAddTask() {
+    this.isAddingTask = true
   }
-
+  onCloseAddTask() {
+    this.isAddingTask = false
+  }
 }
